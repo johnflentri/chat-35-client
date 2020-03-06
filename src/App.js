143686fc4ffe1484eee63093cde1de1d3710899b
johnflentri <jpflentri@gmail.com>
2020-03-06 12:23:48 +0100

    Fully controlled form

--- src/App.js
+++ src/App.js
@@ -2,26 +2,47 @@ import React from 'react';
 import superagent from 'superagent'
 
 class App extends React.Component {
+  state = {
+    text: ''
+  }
+
   onSubmit = async event => {
     event.preventDefault()
 
     try {
       const response = await superagent
         .post('http://localhost:4000/message')
-        .send({ text: 'hardcoded' })
+        .send({ text: this.state.text })
 
       console.log(response)
     } catch (error) {
       console.error(error)
     }
+  }
 
+  onChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  reset = () => {
+    this.setState({ text: '' })
   }
 
   render() {
     return <main>
       <form onSubmit={this.onSubmit}>
-        <input type='text' />
+        <input
+          type='text'
+          onChange={this.onChange}
+          value={this.state.text}
+        />
         <button>Send</button>
+
+        <button onClick={this.reset}>
+          Reset
+          </button>
       </form>
     </main>
   }
