79c28f2a6658fc779a555073ff0849cb57b1fc48
johnflentri <jpflentri@gmail.com>
2020-03-06 11:58:01 +0100

    Hardcode form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import superagent from 'superagent'
 
 class App extends React.Component {
+  onSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      const response = await superagent
+        .post('http://localhost:4000/message')
+        .send({ text: 'hardcoded' })
+
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
   render() {
-    return <div>hello</div>
+    return <main>
+      <form onSubmit={this.onSubmit}>
+        <input type='text' />
+        <button>Send</button>
+      </form>
+    </main>
   }
 }
 
